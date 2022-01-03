9b5611efb22ff0f27982b9dcbd65b0224c138f33
Carol-Student22 <carol.lian22@gmail.com>
2022-01-03 12:09:15 -0500

    testing code

--- lowballd/src/views/Home.vue
+++ lowballd/src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="h-screen w-full bg-gray-600 flex flex-row justify-center items-center">
-        <div class="text-center">
-            <h1 class="text-5xl text-white">lowballd.</h1>
+        <div id="Nav" class="w-full bg-black">
+            <div id="Logo" class="text-center">
+                <h1 class="text-5xl text-white">lowballd.</h1>
+            </div>
+            <div id="Section">
+
+            </div>
         </div>
     </div>
 </template>
