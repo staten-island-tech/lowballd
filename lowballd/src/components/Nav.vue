1c1972853d86e89304467401dd9cb13c130b0009
Carol-Student22 <carol.lian22@gmail.com>
2022-01-03 12:26:21 -0500

    Tailwind working

---
+++ lowballd/src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="nav-page">
+        <a href="#">{{page}}</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Nav",
+    props: ["page"]
+}
+</script>
--- lowballd/src/views/Marketplace.vue
+++ lowballd/src/views/Marketplace.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="marketplace">
         <h1>Marketplace</h1>
+        <div id="nav" class="w-full ">
+            <h1>hi</h1>
+        </div>
     </div>
+
 </template>
+
+<script>
+
+</script>
