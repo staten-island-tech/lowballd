b9ba76cecd5b12c9645e728d96626932267155cc
maxlivit <maxim.livit22@sitechhs.com>
2022-01-12 12:30:01 -0500

    chat

--- lowballd/src/router/index.js
+++ lowballd/src/router/index.js
@@ -2,11 +2,13 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 import Feed from '../views/Feed.vue'
 import Marketplace from '../views/Marketplace.vue'
+import Chat from '../views/Chat.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
     {path: '/feed', name: 'Feed', component: Feed},
     {path: '/marketplace', name: 'Marketplace', component: Marketplace},
+    {path: '/chat', name:"Chat", component: Chat},
 ]
 const router = createRouter({
     history: createWebHistory(),
---
+++ lowballd/src/views/Chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><h1>TEST</h1></div>
+  <Message></Message>
+</template>
+
+<script>
+import Message from "../components/chat/Message.vue";
+
+export default {
+  name: "Chat",
+  components: {
+    Message,
+  },
+};
+</script>
