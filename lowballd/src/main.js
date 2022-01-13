dc4c0add34b3c2291771c0f0494448bd6df0fc3e
alston <alston.chan22@sitechhs.com>
2022-01-13 12:18:03 -0500

    installed vuex

--- lowballd/src/main.js
+++ lowballd/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './tailwind.css'
 import router from './router'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
+Vue.use(Vuex)
 createApp(App).use(router).mount('#app')
