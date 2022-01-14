c4dbedaa4e2d2b2d26cb170d3afa8c6fc8cc7bfe
alston <alston.chan22@sitechhs.com>
2022-01-14 11:56:39 -0500

    Revert "installed vuex"
    This reverts commit dc4c0add34b3c2291771c0f0494448bd6df0fc3e.

--- lowballd/src/main.js
+++ lowballd/src/main.js
@@ -2,8 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './tailwind.css'
 import router from './router'
-import Vue from 'vue'
-import Vuex from 'vuex'
 
-Vue.use(Vuex)
 createApp(App).use(router).mount('#app')
