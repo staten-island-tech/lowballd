e26711bcc25b6e7d867e874c816d96aa34e2e5ef
alston <alston.chan22@sitechhs.com>
2022-01-14 12:11:54 -0500

    animations

--- lowballd/src/components/home/Landing.vue
+++ lowballd/src/components/home/Landing.vue
@@ -1,15 +1,15 @@
 <template>
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
     <div class="h-screen w-full bg-slate-600 flex flex-col justify-around items-center">
         <div class="h-24 w-24"></div>
         <div class="text-center w-3/5">
-            <h1 class="text-9xl text-white font-['Work_Sans'] font-extrabold">lowballd.</h1>
-            <h2 class="p-6 text-xl text-white font-['Work_Sans'] font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+            <h1 class="text-9xl text-white font-['Work_Sans'] font-extrabold animate__animated animate__fadeInDown">lowballd.</h1>
+            <h2 class="p-6 text-xl text-white font-['Work_Sans'] font-semibold animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
         </div>
-        <div class="h-24 w-24 text-white hover:cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-            </svg>
-        </div>
+        <svg class="h-12 w-12 rotate-180 fill-white ease-linear duration-200 hover:cursor-pointer hover:-translate-y-2" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-q</title><path d="M464,464H48a16,16,0,0,1-14.07-23.62l208-384a16,16,0,0,1,28.14,0l208,384A16,16,0,0,1,464,464Z"/></svg>
     </div>
 </template>
 
