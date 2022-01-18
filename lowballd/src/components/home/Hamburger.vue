18d0d73d53068dcdd57d712188229cfe47c994f7
maxlivit <maxim.livit22@sitechhs.com>
2022-01-18 12:31:47 -0500

    first

--- lowballd/src/components/home/Hamburger.vue
+++ lowballd/src/components/home/Hamburger.vue
@@ -1,6 +1,6 @@
 <template>
     <div @click="hamburgerClicked=true" class="absolute w-10 h-10 right-20 top-20 hover:cursor-pointer">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="text-white svg-inline--fa fa-bars fa-w-14 z-50" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
     </div>
     <div v-show="hamburgerClicked">
         <div @click="hamburgerClicked=false" class="fixed left-0 w-4/5 h-screen opacity-50 animate__slideInRight">
