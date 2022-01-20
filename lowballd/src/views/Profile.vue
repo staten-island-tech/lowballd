997306088320de9d37e8109f5241f9dda92bf52a
maxlivit <maxim.livit22@sitechhs.com>
2022-01-20 11:55:21 -0500

    card-profile

--- lowballd/src/views/Profile.vue
+++ lowballd/src/views/Profile.vue
@@ -1,29 +1,18 @@
 <template>
     <Navbar></Navbar>
-    <div class="w-1/5 h-96 m-16">
-         <div class="card text-center shadow-2xl">
-  <div class="avatar mx-auto my-4">
-    <div class="rounded-full w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img src="http://daisyui.com/tailwind-css-component-profile-1@94w.png">
-    </div>
-    </div> 
-    <h1 class="text-2xl">Alston Chan</h1>
-  <button class="btn w-4/5 mx-auto my-2 bg-gray-100 text-slate-900">neutral</button>
-  <button class="btn w-4/5 mx-auto my-2 bg-gray-100 text-slate-900">neutral</button>
-  <button class="btn w-4/5 mx-auto my-2 bg-gray-100 text-slate-900">neutral</button>
-  <button class="btn w-4/5 mx-auto my-2 bg-gray-100 text-slate-900">neutral</button>
-  <button class="btn w-4/5 mx-auto my-2 bg-gray-100 text-slate-900">neutral</button>
-</div> 
-    </div>
+    <Card></Card>
+    <ProfileContent></ProfileContent>
 </template>
 
 <script>
 import Navbar from '../components/profile/Navbar.vue'
+import Card from '../components/profile/Card.vue'
+import ProfileContent from '../components/profile/ProfileContent.vue'
 
 export default {
     name: 'Profile',
     components: {
-        Navbar
+        Navbar, Card, ProfileContent
     },
 }
 </script>
