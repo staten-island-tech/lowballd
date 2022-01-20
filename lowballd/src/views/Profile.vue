d146c5594375a9a935e338c2ec10eaf90ba61ce0
maxlivit <maxim.livit22@sitechhs.com>
2022-01-20 11:55:21 -0500

    card-profile

---
+++ lowballd/src/views/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+    <Navbar></Navbar>
+    <Card></Card>
+    <ProfileContent></ProfileContent>
+</template>
+
+<script>
+import Navbar from '../components/profile/Navbar.vue'
+import Card from '../components/profile/Card.vue'
+import ProfileContent from '../components/profile/ProfileContent.vue'
+
+export default {
+    name: 'Profile',
+    components: {
+        Navbar, Card, ProfileContent
+    },
+}
+</script>
