f0f1b8b439664d3c5c8076efaf9562deed82ddb5
Carol-Student22 <carol.lian22@gmail.com>
2022-01-25 12:27:30 -0500

    done some media queries

--- lowballd/src/components/home/Landing.vue
+++ lowballd/src/components/home/Landing.vue
@@ -8,7 +8,7 @@
             <h1 class="text-9xl text-orange-50 font-['Spectral'] font-bold animate__animated animate__fadeInDown">lowballd.</h1>
             <h2 class="p-6 text-xl text-orange-50 font-['Work_Sans'] font-semibold animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
         </div>
-        <svg class="h-12 w-12 rotate-180 fill-white ease-linear duration-200 hover:cursor-pointer hover:-translate-y-2" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-q</title><path d="M464,464H48a16,16,0,0,1-14.07-23.62l208-384a16,16,0,0,1,28.14,0l208,384A16,16,0,0,1,464,464Z"/></svg>
+        <svg class="h-12 z-50 w-12 rotate-180 fill-white ease-linear duration-200 hover:cursor-pointer hover:-translate-y-2" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><title>ionicons-v5-q</title><path d="M464,464H48a16,16,0,0,1-14.07-23.62l208-384a16,16,0,0,1,28.14,0l208,384A16,16,0,0,1,464,464Z"/></svg>
     </div>
     </div>
 </template>
