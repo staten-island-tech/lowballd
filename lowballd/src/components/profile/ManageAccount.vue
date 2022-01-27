50f0e91f2b0e61187f15aae644fd9e914d4aac3d
maxlivit <maxim.livit22@sitechhs.com>
2022-01-27 12:07:30 -0500

    profile

--- lowballd/src/components/profile/ManageAccount.vue
+++ lowballd/src/components/profile/ManageAccount.vue
@@ -1,12 +1,12 @@
 <template>
-<div>
+<div v-if="!manageAccountToggled">
     <h1 class="text-2xl m-10 font-bold">Email</h1>
     <p class="mx-10 -my-8 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     <h1 class="text-xl font-semibold mx-10 mt-14">Change Email</h1>
     <h1 class=
     "text-base font-light mx-14 mt-2">Current Email:<input class="ml-1.5" type="text" placeholder="jonny.smith@gmail.com"></h1>
     <h1 class=
-    "text-base font-light mx-14 mt-2">New Email:<input class="ml-1.5 w-48" type="text" placeholder="JonnySmith11@gmail.com"></h1>
+    "text-base font-light mx-16 mt-2">New Email:<input class="ml-1.5 w-48" type="text" placeholder="JonnySmith11@gmail.com"></h1>
     <h1 class= 
     "text-base font-light mx-14 mt-2">Password:<input class="ml-1.5" type="text" placeholder="xL1!4?78ax"></h1>
     <h1 class="text-2xl m-10 font-bold">Password</h1>
@@ -24,5 +24,11 @@
 <script>
     export default {
         name: 'ManageAccount',
+        data() {
+            return {
+                manageAccountToggled: false;
+            }
     }
+    }
+    
 </script>
