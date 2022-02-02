4ab72c4e68646c0fae7f2b6d6a1d73503b780191
Carol-Student22 <carol.lian22@gmail.com>
2022-02-02 12:37:48 -0500

    Filter needs to be centered

--- lowballd/src/components/marketplace/Filter-Btn.vue
+++ lowballd/src/components/marketplace/Filter-Btn.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="hover:bg-gray-400 flex justify-center items-center btn-ghost btn-sm cursor-pointer rounded-btn bg-white mx-1 my-1.5 capitalize">
+    <a class="hover:bg-gray-400 flex justify-center items-center btn-ghost btn-sm cursor-pointer rounded-btn bg-white my-2 capitalize">
         <h2 >{{filterBtn}}</h2>
     </a>
 </template>
--- lowballd/src/components/marketplace/Filter.vue
+++ lowballd/src/components/marketplace/Filter.vue
@@ -1,45 +1,41 @@
 <template>
-    <div class="w-60 sm:w-full sm:flex sm:flex-col sm:items-center capitalize ">
-       <div class="sm:w-10/12 h-auto bg-gray-200 p-3 mb-5 rounded-xl ">
-           <div class="flex flex-col">
-               <FilterCategory filterCategory='sort'/>
-                <FilterBtn filterBtn='sort'/>
-                <FilterBtn filterBtn='sort2'/>
-           </div>
-       </div>
-       <div class="sm:w-10/12 h-auto bg-gray-200 p-3 mb-5 rounded-xl ">
-           <div class="flex flex-col">
-               <FilterCategory filterCategory='Filler'/>
-                <FilterBtn filterBtn='hi'/>
-                <FilterBtn filterBtn='hi'/>
-           </div>
-           <div class="flex flex-col">
-               <FilterCategory filterCategory='Filler'/>
-                <FilterBtn filterBtn='hi'/>
-                <FilterBtn filterBtn='hi'/>
-           </div>
-           <div class="flex flex-col">
-               <FilterCategory filterCategory='Color'/>
-                <FilterBtn filterBtn='Blue'/>
-                <FilterBtn filterBtn='Beige'/>
-           </div>
-           <div class="flex flex-col">
-               <FilterCategory filterCategory='Price'/>
-                <FilterBtn filterBtn='High to Low'/>
-                <FilterBtn filterBtn='Low to High'/>
-           </div>
-       </div>
-       <div tabindex="0" class="collapse w-96 border rounded-box border-base-300 collapse-arrow"> 
-        <div class="collapse-title text-xl font-medium">
-            I open with focus
-        </div> 
-        <div class="collapse-content"> 
-            <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox instead of focus. Or you can force-open/force-close using 
-            <span class="badge badge-outline">collapse-open</span> and 
-            <span class="badge badge-outline">collapse-close</span> classes.
-            </p>
+    <div class="w-60 h-auto bg-gray-100 rounded-xl capitalize sm:w-full sm:m-0 sm:justify-center sm:w-10/12 " >
+        <div class='flex flex-col sm:justify-center'>
+            <div class="collapse collapse-arrow w-full rounded-xl">
+                <input type="checkbox"> 
+                <div class="collapse-title">
+                    <FilterCategory filterCategory='Color'/>
+                </div> 
+                <div class="collapse-content"> 
+                    <FilterBtn filterBtn='Beige'/>
+                    <FilterBtn filterBtn='Beige'/>
+                    <FilterBtn filterBtn='Beige'/>
+                </div>
+            </div> 
+            <div class="collapse collapse-arrow w-full rounded-xl">
+                <input type="checkbox"> 
+                <div class="collapse-title">
+                    <FilterCategory filterCategory='Color'/>
+                </div> 
+                <div class="collapse-content"> 
+                    <FilterBtn filterBtn='Beige'/>
+                    <FilterBtn filterBtn='Beige'/>
+                    <FilterBtn filterBtn='Beige'/>
+                </div>
+            </div> 
+            <div class="collapse collapse-arrow w-full rounded-xl">
+                <input type="checkbox"> 
+                <div class="collapse-title">
+                    <FilterCategory filterCategory='Color'/>
+                </div> 
+                <div class="collapse-content"> 
+                    <FilterBtn filterBtn='Beige'/>
+                    <FilterBtn filterBtn='Beige'/>
+                    <FilterBtn filterBtn='Beige'/>
+                </div>
+            </div> 
         </div>
-        </div> 
+
     </div>
 
 </template>
