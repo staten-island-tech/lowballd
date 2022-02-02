7eed90be849d782f390fb6d66835b2726a9a4bd4
Carol-Student22 <carol.lian22@gmail.com>
2022-02-02 12:09:42 -0500

    Testing stuff from daisyUI

--- lowballd/src/components/marketplace/Filter.vue
+++ lowballd/src/components/marketplace/Filter.vue
@@ -29,6 +29,17 @@
                 <FilterBtn filterBtn='Low to High'/>
            </div>
        </div>
+       <div tabindex="0" class="collapse w-96 border rounded-box border-base-300 collapse-arrow"> 
+        <div class="collapse-title text-xl font-medium">
+            I open with focus
+        </div> 
+        <div class="collapse-content"> 
+            <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox instead of focus. Or you can force-open/force-close using 
+            <span class="badge badge-outline">collapse-open</span> and 
+            <span class="badge badge-outline">collapse-close</span> classes.
+            </p>
+        </div>
+        </div> 
     </div>
 
 </template>
