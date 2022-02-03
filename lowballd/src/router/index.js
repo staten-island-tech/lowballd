d942f01a6e42dfbfac3729136d3cc83dd4428f35
maxlivit <maxim.livit22@sitechhs.com>
2022-02-03 12:32:57 -0500

    mainprofile

--- lowballd/src/router/index.js
+++ lowballd/src/router/index.js
@@ -3,7 +3,8 @@ import Home from "../views/Home.vue";
 import Feed from "../views/Feed.vue";
 import Marketplace from "../views/Marketplace.vue";
 import About from "../views/About.vue";
-import Login from "../views/Login.vue"
+import Login from "../views/Login.vue";
+import Profile from "../views/Profile.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
@@ -11,6 +12,7 @@ const routes = [
   { path: "/marketplace", name: "Marketplace", component: Marketplace },
   { path: "/about", name: "About", component: About },
   { path: "/login", name: "Login", component: Login },
+  { path: "/profile", name: "Profile", component: Profile },
 ];
 const router = createRouter({
   history: createWebHistory(),
