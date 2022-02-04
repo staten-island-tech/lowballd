3126826685869043f07d56760f82fc47e027c58d
Carol-Student22 <carol.lian22@gmail.com>
2022-02-04 12:17:11 -0500

    Hamburger Media Queries - Complete

--- lowballd/src/components/home/Hamburger.vue
+++ lowballd/src/components/home/Hamburger.vue
@@ -1,6 +1,6 @@
 <template>
-    <div @click="hamburgerClicked=true" class="sm:fixed sm:rounded-xl sm:right-10 sm:top-10 z-10 text-white absolute w-10 h-10 right-20 top-20 hover:cursor-pointer">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="z-20 svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
+    <div @click="hamburgerClicked=true" class="sm:bg-black sm:fixed sm:rounded-full sm:right-10 sm:top-10 z-10 text-white absolute w-12 h-12 right-20 top-20 hover:cursor-pointer">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="sm:p-3 z-20 svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
     </div>
     <div v-show="hamburgerClicked">
         <div @click="hamburgerClicked=false" class="fixed left-0 w-4/5 h-screen opacity-50 animate__slideInRight">
