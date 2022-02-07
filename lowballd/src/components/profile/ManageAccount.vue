1563db316d5c64cb36ee87db8c3011f1fc3929b3
maxlivit <maxim.livit22@sitechhs.com>
2022-02-07 12:30:14 -0500

    bepbioe

--- lowballd/src/components/profile/ManageAccount.vue
+++ lowballd/src/components/profile/ManageAccount.vue
@@ -1,23 +1,13 @@
 <template>
 <div>
-    <h1 class="text-2xl m-10 font-bold">Email</h1>
-    <p class="mx-10 -my-8 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <h1 class="text-xl font-semibold mx-10 mt-14">Change Email</h1>
-    <h1 class=
-    "text-base font-light mx-14 mt-2">Current Email:<input class="ml-1.5" type="text" placeholder="jonny.smith@gmail.com"></h1>
-    <h1 class=
-    "text-base font-light mx-16 mt-2">New Email:<input class="ml-1.5 w-48" type="text" placeholder="JonnySmith11@gmail.com"></h1>
-    <h1 class= 
-    "text-base font-light mx-14 mt-2">Password:<input class="ml-1.5" type="text" placeholder="xL1!4?78ax"></h1>
-    <h1 class="text-2xl m-10 font-bold">Password</h1>
-    <p class="mx-10 -my-8 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <h1 class="text-xl font-semibold mx-10 mt-14">Change Password</h1>
-    <h1 class=
-    "text-base font-light mx-14 mt-2">Current Password:<input class="ml-1.5" type="text" placeholder="xL1!4?78ax"></h1>
-    <h1 class=
-    "text-base font-light mx-14 mt-2">New Password:<input class="ml-1.5 w-48" type="text" placeholder="Strong Password"></h1>
-    <h1 class= 
-    "text-base font-light mx-14 mt-2">Re-Enter Password:<input class="ml-1.5" type="text" placeholder="Strong Password"></h1>
+    <h1 class="text-2xl m-10 font-bold">Description</h1>
+    <p class="mx-10 -my-8 text-base text-gray-400">Write a little joke or greeting for any passerbys.</p>
+    <input class="mx-10 my-12 w-11/12 h-32" type="text" placeholder="Description">
+    <h1 class="text-2xl m-10 font-bold">Blocked Accounts</h1>
+    <p class="mx-10 -my-8 text-base text-gray-400">The following usernames are blocked.</p>
+    <h1 class="text-2xl font-bold mx-10 mt-14">Recieve Emails</h1>
+     <p class="mx-10 text-base text-gray-400">Mainly on updates and new posts.</p>
+   
 </div>
 </template>
 
@@ -25,5 +15,4 @@
     export default {
         name: 'ManageAccount',
     }
-    
 </script>
