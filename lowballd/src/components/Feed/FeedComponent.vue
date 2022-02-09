0a9e15480c64820176736377f3f2445c85755ff4
vincentvvs <vincent.shi22@sitechhs.com>
2022-02-09 12:27:25 -0500

    ok
    ok

---
+++ lowballd/src/components/Feed/FeedComponent.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<h1 class="text-center text-3xl mt-20 font-['Spectral'] font-semibold">OUTFIT OF THE DAY</h1>
+<h1 class="text-center text-3xl font-['SPectral'] font-semibold">WHAT DID YOU WEAR TODAY?</h1>
+<div class="flex items-center justify-center"><button class="mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current hover:font-bold cursor-pointer select-none">Trending</button><button class="mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current cursor-pointer hover:font-bold select-none">Following</button></div>
+</div>
+
+<div class="w-1/5 rounded p-0 bg-slate-100 h-[30rem] mt-[10rem] ml-20">BOX</div>
+</template>
+
+<script>
+    export default {
+        name: 'FeedComponent',
+    }
+
+</script>
--- lowballd/src/views/Feed.vue
+++ lowballd/src/views/Feed.vue
@@ -1,24 +1,27 @@
 <template>
 <FeedNavbar></FeedNavbar>
-<div>
-<h1 class="text-center text-3xl mt-20 font-['Spectral'] font-semibold">OUTFIT OF THE DAY</h1>
-<h1 class="text-center text-3xl font-['SPectral'] font-semibold">WHAT DID YOU WEAR TODAY?</h1>
-<div class="flex items-center justify-center"><button class="mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current hover:font-bold cursor-pointer select-none">Trending</button><button class="mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current cursor-pointer hover:font-bold select-none">Following</button></div>
+<FeedComponent></FeedComponent>
 
-</div>
-<div class="w-1/3 rounded p-0 border-black bg-slate-600 mt-20 ml-20">BOX</div>
+
 
 </template>
 
 <script>
 import FeedNavbar from '../components/Feed/FeedNavbar.vue'
+import FeedComponent from '../components/Feed/FeedComponent.vue'
 export default {
-    name: 'Feed',
+    name: 'Home',
     components: {
-        FeedNavbar
+        FeedNavbar, FeedComponent
     },
 }
+
+
+
+
 </script>
+
+
 
 
 
