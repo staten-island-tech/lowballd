c9ae1a29e4369e8e984068fc7c6a2ecff6783c2c
Carol-Student22 <carol.lian22@gmail.com>
2022-02-11 12:17:10 -0500

    Skeleton Layout - Very Bland and Simple
    Outline of how things will work. Additional things will be added or removed to fit the site

--- lowballd/src/views/Home.vue
+++ lowballd/src/views/Home.vue
@@ -3,6 +3,7 @@
     <Landing></Landing>
     <About></About>
     <Market></Market>
+    <Footer></Footer>
 </template>
 
 <script>
@@ -10,11 +11,12 @@ import Landing from '../components/home/Landing.vue'
 import About from '../components/home/About.vue'
 import Market from '../components/home/Market.vue'
 import Navbar from '../components/home/Navbar.vue'
+import Footer from '../components/footer/Footer.vue'
 
 export default {
     name: 'Home',
     components: {
-        Landing, About, Market, Navbar
+        Landing, About, Market, Navbar, Footer
     },
 }
 </script>
