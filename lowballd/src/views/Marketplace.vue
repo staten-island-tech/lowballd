3ca4f0bdc6ee8d60e527f94a083a46515040a1f4
Carol-Student22 <carol.lian22@gmail.com>
2022-02-11 12:26:33 -0500

    Testing

--- lowballd/src/views/Marketplace.vue
+++ lowballd/src/views/Marketplace.vue
@@ -1,15 +1,17 @@
 <template>
       <Navbar></Navbar> 
       <MarketLanding></MarketLanding>
+      <Footer2></Footer2>
 </template>
 
 <script>
 import Navbar from '../components/marketplace/Navbar.vue'
 import MarketLanding from '../components/marketplace/Market-Landing.vue'
+import Footer2 from '../components/footer/Footer2.vue'
 export default {
   name: 'Marketplace',
   components: {
-    Navbar, MarketLanding,
+    Navbar, MarketLanding, Footer2,
   },
 }
 </script>
