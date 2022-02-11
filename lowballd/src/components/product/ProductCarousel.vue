6391171d877d635522f6d31e0a284412f4fc76d7
maxlivit <maxim.livit22@sitechhs.com>
2022-02-11 12:30:19 -0500

    oml

--- lowballd/src/components/product/ProductCarousel.vue
+++ lowballd/src/components/product/ProductCarousel.vue
@@ -1,26 +1,23 @@
 <template>
-<div class="max-w-4xl  p-4 mt-12 ml-12 space-x-4 carousel carousel-center bg-neutral rounded-box">
-  <div class="carousel-item">
-    <img src="https://picsum.photos/id/500/256/144" class="rounded-box w-full h-96">
+<div class="w-full carousel">
+  <div id="item1" class="w-full pt-20 carousel-item">
+    <img src="https://picsum.photos/id/500/800/300" class="w-1/3">
   </div> 
-  <div class="carousel-item">
-    <img src="https://picsum.photos/id/501/256/144" class="rounded-box w-full h-96">
+  <div id="item2" class="w-full pt-20 carousel-item">
+    <img src="https://picsum.photos/id/501/800/300" class="w-1/3">
   </div> 
-  <div class="carousel-item">
-    <img src="https://picsum.photos/id/502/256/144" class="rounded-box w-full h-96">
+  <div id="item3" class="w-full pt-20 carousel-item">
+    <img src="https://picsum.photos/id/502/800/300" class="w-1/3">
   </div> 
-  <div class="carousel-item">
-    <img src="https://picsum.photos/id/503/256/144" class="rounded-box w-full h-96">
-  </div> 
-  <div class="carousel-item">
-    <img src="https://picsum.photos/id/504/256/144" class="rounded-box w-full h-96">
-  </div> 
-  <div class="carousel-item">
-    <img src="https://picsum.photos/id/505/256/144" class="rounded-box w-full h-96">
-  </div> 
-  <div class="carousel-item">
-    <img src="https://picsum.photos/id/506/256/144" class="rounded-box w-full h-96">
+  <div id="item4" class="w-full pt-20 carousel-item">
+    <img src="https://picsum.photos/id/503/800/300" class="w-1/3">
   </div>
+</div> 
+<div class="flex justify-center w-1/3 py-4 space-x-2">
+  <a href="/components/carousel#item1" class="btn btn-xs btn-circle">1</a> 
+  <a href="/components/carousel#item2" class="btn btn-xs btn-circle">2</a> 
+  <a href="/components/carousel#item3" class="btn btn-xs btn-circle">3</a> 
+  <a href="/components/carousel#item4" class="btn btn-xs btn-circle">4</a>
 </div>
 </template>
 
