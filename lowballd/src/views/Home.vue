e904ba92984cb581dae8629d05cacd7d56c97cdf
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2022-02-28 12:21:38 -0500

    trying to integrate login to only a button instead of page

--- lowballd/src/views/Home.vue
+++ lowballd/src/views/Home.vue
@@ -1,20 +1,27 @@
 <template>
-    <Navbar></Navbar>
-    <Landing></Landing>
-    <About></About>
-    <Market></Market>
+  <Navbar><Login></Login></Navbar>
+
+  <Landing></Landing>
+  <About></About>
+
+  <Market></Market>
 </template>
 
 <script>
-import Landing from '../components/home/Landing.vue'
-import About from '../components/home/About.vue'
-import Market from '../components/home/Market.vue'
-import Navbar from '../components/home/Navbar.vue'
+import Landing from "../components/home/Landing.vue";
+import About from "../components/home/About.vue";
+import Market from "../components/home/Market.vue";
+import Navbar from "../components/home/Navbar.vue";
+import Login from "../components/login/LoginComponent.vue";
 
 export default {
-    name: 'Home',
-    components: {
-        Landing, About, Market, Navbar
-    },
-}
+  name: "Home",
+  components: {
+    Landing,
+    About,
+    Market,
+    Navbar,
+    Login,
+  },
+};
 </script>
