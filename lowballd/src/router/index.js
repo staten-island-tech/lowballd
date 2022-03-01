90eac6df805e202497f3b4f5a0bfd27c04f02264
Carol-Student22 <carol.lian22@gmail.com>
2022-03-01 11:51:52 -0500

    Revert "Merge branch 'main' into Footer"
    This reverts commit c159b74f817a629929d6528e0febb4b05bcf086a, reversing
    changes made to fa28b4eb29455ce43e2ed589011b1b846cf43bd8.

--- lowballd/src/router/index.js
+++ lowballd/src/router/index.js
@@ -3,9 +3,7 @@ import Home from "../views/Home.vue";
 import Feed from "../views/Feed.vue";
 import Marketplace from "../views/Marketplace.vue";
 import About from "../views/About.vue";
-import Login from "../views/Login.vue";
-import Profile from "../views/Profile.vue";
-import Product from "../views/Product.vue";
+import Login from "../views/Login.vue"
 
 const routes = [
   { path: "/", name: "Home", component: Home },
@@ -13,8 +11,6 @@ const routes = [
   { path: "/marketplace", name: "Marketplace", component: Marketplace },
   { path: "/about", name: "About", component: About },
   { path: "/login", name: "Login", component: Login },
-  { path: "/profile", name: "Profile", component: Profile },
-  { path: "/product", name: "Product", component: Product },
 ];
 const router = createRouter({
   history: createWebHistory(),
--- lowballd/src/views/Login.vue
+++ lowballd/src/views/Login.vue
@@ -1,27 +1,6 @@
-<script setup>
-import { useAuth0, AuthState } from "../utils/useAuth0";
-const { login, logout, initAuth } = useAuth0(AuthState);
-
-initAuth();
-</script>
-
 <template>
-  <div v-if="!AuthState.loading">
-    <div v-if="!AuthState.isAuthenticated">
-      <button @click="login()" class="btn btn-primary">Login</button>
-    </div>
-
-    <div v-else>
-      <p> Welcome to VueAuth <strong>{{ AuthState.user.name }}</strong></p>
-      <button @click="logout()" class="btn btn-secondary">Logout</button>
-    </div>
-  </div>
-
-  <div v-else>
-    Loading ...
-  </div>
+    <LoginComponent></LoginComponent>
 </template>
-
 <script>
 import LoginComponent from '../components/login/LoginComponent.vue'
 
@@ -32,37 +11,3 @@ export default {
     },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.btn {
-  padding: 8px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 1.5;
-  border: none;
-  cursor: pointer;
-  min-width: 100px;
-  border-radius: 4px;
-  font-weight: bold;
-}
-
-.btn-primary {
-  background: #41B883;
-  color: white;
-}
-
-.btn-secondary {
-  background: #aaa;
-  color: white;
-}
-</style>
