8c3cd91e0befc3a2f5e26b76e283f6b485bf9fea
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2022-03-07 12:27:55 -0500

    working on a new guide

--- lowballd/src/main.js
+++ lowballd/src/main.js
@@ -1,6 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './tailwind.css'
-import router from './router'
+import Vue from "vue";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./tailwind.css";
+import router from "./router";
+import { domain, clientId } from "@/auth_config.json";
 
-createApp(App).use(router).mount('#app')
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
+
+Vue.config.productionTip = false;
+
+createApp(App).use(router).mount("#app");
