c1053cf40d129578d6388f1638cabb1eb7f64979
alston <alston.chan22@sitechhs.com>
2022-03-08 10:39:21 -0500

    redo

--- lowballd/src/main.js
+++ lowballd/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './tailwind.css'
 import router from './router'
+import { TroisJSVuePlugin } from 'troisjs';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(TroisJSVuePlugin).mount('#app')
