7e62f806ae4adf57a8f0e41058f96fac6f9f719c
vincentvvs <vincent.shi22@sitechhs.com>
2022-03-09 12:29:21 -0500

    ok

--- lowballd/src/components/Feed/FeedComponent.vue
+++ lowballd/src/components/Feed/FeedComponent.vue
@@ -4,109 +4,157 @@
 <h1 class="text-center text-3xl font-['SPectral'] font-semibold">WHAT DID YOU WEAR TODAY?</h1>
 <div class="flex items-center justify-center"><button class=" mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current hover:font-bold cursor-pointer select-none">Trending</button><button class="mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current cursor-pointer hover:font-bold select-none">Following</button></div>
 </div>
-<div class="flex justify-between flex-wrap">
-    <a class="block" href="/product/small-headphones">
-  <img
-    class="object-cover w-full h-[350px] sm:h-[450px]"
-    src="https://images.unsplash.com/photo-1504274066651-8d31a536b11a"
-    alt="Small Headphones"
+<div class=" flex justify-between">
+<a class="relative block border border-gray-100" href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
   >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
 
-  <div class="mt-2 space-y-3">
-    <p class="text-xs text-gray-500">
-      Space Grey
+  <img
+    class="object-contain w-full h-56 lg:h-72"
+    src="https://www.hyperui.dev/photos/toy-1.jpeg"
+    alt="Build Your Own Drone"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <strong class="inline-block px-3 py-1 text-xs font-medium bg-yellow-400">
+      New
+    </strong>
+
+    <h5 class="mt-4 text-lg font-bold">
+      Build Your Own Drone
+    </h5>
+
+    <p class="mt-2 text-sm text-gray-700">
+      $14.99
     </p>
 
-    <div class="flex space-x-1">
-    </div>
-
-    <div class="flex justify-between text-xs">
-      <p> Small Headphones </p>
-
-      <p> $299 </p>
-    </div>
+    <button
+      class="block w-full p-4 mt-4 text-sm font-medium bg-yellow-500 rounded-sm"
+      type="button"
+    >
+      Add to Cart
+    </button>
   </div>
-</a>
-<a class="block" href="/product/small-headphones">
-  <img
-    class="object-cover w-full h-[350px] sm:h-[450px]"
-    src="https://images.unsplash.com/photo-1504274066651-8d31a536b11a"
-    alt="Small Headphones"
+</a><a class="relative block border border-gray-100" href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
   >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
 
-  <div class="mt-2 space-y-3">
-    <p class="text-xs text-gray-500">
-      Space Grey
+  <img
+    class="object-contain w-full h-56 lg:h-72"
+    src="https://www.hyperui.dev/photos/toy-1.jpeg"
+    alt="Build Your Own Drone"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <strong class="inline-block px-3 py-1 text-xs font-medium bg-yellow-400">
+      New
+    </strong>
+
+    <h5 class="mt-4 text-lg font-bold">
+      Build Your Own Drone
+    </h5>
+
+    <p class="mt-2 text-sm text-gray-700">
+      $14.99
     </p>
 
-    <div class="flex space-x-1">
-      
-        <span class="sr-only"> Space Grey </span>
-
-        
-    </div>
-
-    <div class="flex justify-between text-xs">
-      <p> Small Headphones </p>
-
-      <p> $299 </p>
-    </div>
+    <button
+      class="block w-full p-4 mt-4 text-sm font-medium bg-yellow-500 rounded-sm"
+      type="button"
+    >
+      Add to Cart
+    </button>
   </div>
-</a>
-<a class="block" href="/product/small-headphones">
-  <img
-    class="object-cover w-full h-[350px] sm:h-[450px]"
-    src="https://images.unsplash.com/photo-1504274066651-8d31a536b11a"
-    alt="Small Headphones"
+</a><a class="relative block border border-gray-100" href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
   >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
 
-  <div class="mt-2 space-y-3">
-    <p class="text-xs text-gray-500">
-      Space Grey
+  <img
+    class="object-contain w-full h-56 lg:h-72"
+    src="https://www.hyperui.dev/photos/toy-1.jpeg"
+    alt="Build Your Own Drone"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <strong class="inline-block px-3 py-1 text-xs font-medium bg-yellow-400">
+      New
+    </strong>
+
+    <h5 class="mt-4 text-lg font-bold">
+      Build Your Own Drone
+    </h5>
+
+    <p class="mt-2 text-sm text-gray-700">
+      $14.99
     </p>
 
-    <div class="flex space-x-1">
-      
-        <span class="sr-only"> Space Grey </span>
-
-        
-    </div>
-
-    <div class="flex justify-between text-xs">
-      <p> Small Headphones </p>
-
-      <p> $299 </p>
-    </div>
+    <button
+      class="block w-full p-4 mt-4 text-sm font-medium bg-yellow-500 rounded-sm"
+      type="button"
+    >
+      Add to Cart
+    </button>
   </div>
-</a>
-<a class="block" href="/product/small-headphones">
-  <img
-    class="object-cover w-full h-[350px] sm:h-[450px]"
-    src="https://images.unsplash.com/photo-1504274066651-8d31a536b11a"
-    alt="Small Headphones"
+</a><a class="relative block border border-gray-100" href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
   >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
 
-  <div class="mt-2 space-y-3">
-    <p class="text-xs text-gray-500">
-      Space Grey
+  <img
+    class="object-contain w-full h-56 lg:h-72"
+    src="https://www.hyperui.dev/photos/toy-1.jpeg"
+    alt="Build Your Own Drone"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <strong class="inline-block px-3 py-1 text-xs font-medium bg-yellow-400">
+      New
+    </strong>
+
+    <h5 class="mt-4 text-lg font-bold">
+      Build Your Own Drone
+    </h5>
+
+    <p class="mt-2 text-sm text-gray-700">
+      $14.99
     </p>
 
-    <div class="flex space-x-1">
-      
-        <span class="sr-only"> Space Grey </span>
-
-        
-    </div>
-
-    <div class="flex justify-between text-xs">
-      <p> Small Headphones </p>
-
-      <p> $299 </p>
-    </div>
+    <button
+      class="block w-full p-4 mt-4 text-sm font-medium bg-yellow-500 rounded-sm"
+      type="button"
+    >
+      Add to Cart
+    </button>
   </div>
 </a>
 </div>
-
 
 
 </template>
