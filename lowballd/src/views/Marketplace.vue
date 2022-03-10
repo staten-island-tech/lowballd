62ce1ce532ac76812f52d0e14278d46c6eb5f214
Carol-Student22 <carol.lian22@gmail.com>
2022-03-10 12:30:14 -0500

    Remember to host on node terminal thing on computer otherwise the fake server doesnt work

--- lowballd/src/views/Marketplace.vue
+++ lowballd/src/views/Marketplace.vue
@@ -8,12 +8,27 @@
 import Navbar from "../components/marketplace/Navbar.vue";
 import MarketLanding from "../components/marketplace/Market-Landing.vue";
 import Footer from "../components/footer/Footer.vue";
+import axios from 'axios';
 export default {
   name: "Marketplace",
   components: {
     Navbar,
     MarketLanding,
     Footer,
+  },
+  data() {
+      return {
+          profile_data: [],
+      }
+  },
+  async created() {
+      try {
+      const res = await axios.get(`http://localhost:3001/profiledata`);
+          this.profile_data = res.data[0];
+          console.log(res.data[0])
+      } catch (error) {
+          console.log(error);
+      }
   },
 };
 </script>
