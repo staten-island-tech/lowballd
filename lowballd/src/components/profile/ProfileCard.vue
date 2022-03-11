61b337a45945f777b56c71f61397e08bd04814c3
alston <alston.chan22@sitechhs.com>
2022-03-11 12:01:22 -0500

    added to profile db

--- lowballd/src/components/profile/ProfileCard.vue
+++ lowballd/src/components/profile/ProfileCard.vue
@@ -4,11 +4,11 @@
     <div class="my-2"></div>
         <div class="avatar mx-auto my-4">
             <div class="rounded-full w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2">
-        <img :src='profile_data.profile_picture'>
+        <img :src='profile_data.user_picture'>
             </div>
         </div> 
-    <h1 class="text-2xl">{{ profile_data.name }}</h1>
-    <h1 class="text-base">{{ profile_data.email }}</h1>
+    <h1 class="text-2xl">{{ profile_data.user_fullname }}</h1>
+    <h1 class="text-base">{{ profile_data.user_email }}</h1>
     <div class="py-6">
         <button class="btn btn-sm w-4/5 mx-auto my-2 bg-gray-100 text-slate-900 hover:bg-sky-100">Posts</button>
         <button class="btn btn-sm w-4/5 mx-auto my-2 bg-gray-100 text-slate-900 hover:bg-sky-100">Manage Account</button>
