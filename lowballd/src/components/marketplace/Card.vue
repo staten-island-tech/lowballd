3af4914f1f23c112017a1585029ec6a8254a69b8
Carol-Student22 <carol.lian22@gmail.com>
2022-03-16 12:08:13 -0400

    routed card to product

--- lowballd/src/components/marketplace/Card.vue
+++ lowballd/src/components/marketplace/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="hover:bg-slate-200 cursor-pointer m-1">
+    <router-link :to="'/product/'"   class="hover:bg-slate-200 cursor-pointer m-1">
         <div class="flex flex-col w-60 m-3">
             <div class="card-photo">
                 <div class="w-full h-[21rem]">
@@ -9,7 +9,7 @@
             <h2 class="my-1">{{name}}</h2>
             <h2>${{price}}</h2>
         </div>
-    </a>
+    </router-link>
 </template>
 
 <script>
@@ -20,6 +20,6 @@ export default {
         price: String,
         imageUrl: String,
     },
-    
 }
+    
 </script>
