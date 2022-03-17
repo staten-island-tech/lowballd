c150017e5772968781daf0ae44e3abbbdc255636
alston <alston.chan22@sitechhs.com>
2022-03-17 12:08:13 -0400

    fixed up styling for marketplace card

--- lowballd/src/components/marketplace/Card.vue
+++ lowballd/src/components/marketplace/Card.vue
@@ -2,11 +2,11 @@
     <a class="hover:bg-slate-200 cursor-pointer m-1">
         <div class="flex flex-col w-60 m-3">
             <div class="card-photo">
-                <div class="w-full h-[21rem]">
-                    <img class="w-full h-[21rem]" :src="imageUrl">
+                <div class="w-full h-60">
+                    <img class="h-full w-auto object-cover" :src="imageUrl">
                 </div>
             </div>
-            <h2 class="my-1">{{name}}</h2>
+            <h2 class="mt-2 my-1">{{name}}</h2>
             <h2>${{price}}</h2>
         </div>
     </a>
