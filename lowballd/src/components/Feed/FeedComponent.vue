a20a9898339290e577b7f138baca51e3959d8324
vincentvvs <vincent.shi22@sitechhs.com>
2022-03-17 12:13:58 -0400

    ok

--- lowballd/src/components/Feed/FeedComponent.vue
+++ lowballd/src/components/Feed/FeedComponent.vue
@@ -4,8 +4,8 @@
 <h1 class="text-center text-3xl font-['SPectral'] font-semibold">WHAT DID YOU WEAR TODAY?</h1>
 <div class="flex items-center justify-center"><button class=" mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current hover:font-bold cursor-pointer select-none">Trending</button><button class="mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current cursor-pointer hover:font-bold select-none">Following</button></div>
 </div>
-<div class=" flex space-between">
-<a class="relative block border bg-slate-200 ml-[2rem]" href="/product/build-your-own-drone">
+<div class="flex justify-between flex-wrap">
+<a class="relative block border bg-slate-200 ml-[5rem] w-1/6" href="/product/build-your-own-drone">
   <button
     class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
     type="button"
@@ -24,16 +24,13 @@
 
   <div class="p-6">
     <h1>@KESHI</h1>
-    <h5 class="mt-4 text-lg font-bold">
     
-      LIKE I NEED YOU
-    </h5>
 
     
 
     
   </div>
-</a><a class="relative block border bg-slate-200 border-gray-100" href="/product/build-your-own-drone">
+</a><a class="relative block border bg-slate-200 border-gray-100 w-1/6" href="/product/build-your-own-drone">
   <button
     class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
     type="button"
@@ -52,9 +49,7 @@
 
   <div class="p-6">
     <h1>@alstn</h1>
-    <h5 class="mt-4 text-lg font-bold">
-      ON OFF ON OFF
-    </h5>
+    
 
     
 
@@ -62,7 +57,7 @@
 
     
   </div>
-</a><a class="relative block border bg-slate-200 " href="/product/build-your-own-drone">
+</a><a class="relative block border bg-slate-200 w-1/6 " href="/product/build-your-own-drone">
   <button
     class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
     type="button"
@@ -74,21 +69,24 @@
 
   <img
     class="object-contain w-full h-30 lg:h-72 mt-[4rem]"
-    src="https://www.highsnobiety.com/static-assets/thumbor/SXbsUkCaRWjLAt7gYKByA8AagBA=/1600x2400/www.highsnobiety.com/static-assets/wp-content/uploads/2021/12/08155944/adam-s-04.jpg"
+    src="https://i.pinimg.com/originals/d8/67/4a/d8674a00506bcecfe82af05dcbb0cf17.jpg"
     alt="Build Your Own Drone"
     loading="lazy"
   />
+
   <div class="p-6">
     <h1>@alstn</h1>
-    <h5 class="mt-4 text-lg font-bold">
-      ON OFF ON OFF
-    </h5>
+    
+    
 
-  
+    
+
+    
 
     
   </div>
-</a><a class="relative block border bg-slate-200 mr-[2rem]" href="/product/build-your-own-drone">
+</a>
+<a class="relative block border bg-slate-200 mr-[5rem] w-1/6" href="/product/build-your-own-drone">
   <button
     class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
     type="button"
@@ -100,19 +98,133 @@
 
   <img
     class="object-contain w-full h-30 lg:h-72 mt-[4rem]"
-    src=""
+    src="https://i.pinimg.com/originals/c8/b6/d2/c8b6d20be240aa6e8667d9f383d9ed3a.jpg"
     alt="Build Your Own Drone"
     loading="lazy"
   />
 
   <div class="p-6">
+    <h1>@alstn</h1>
+    
     
 
     
 
-    <p class="mt-2 text-sm text-gray-700">
-      $14.99
-    </p>
+    
+
+    
+  </div>
+</a>
+</div>
+
+
+
+<div class="flex justify-between flex-wrap mt-[5rem]">
+<a class="relative block border bg-slate-200 ml-[5rem] w-1/6" href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
+  >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
+
+  <img 
+    src="https://lastfm.freetls.fastly.net/i/u/770x0/0e62a68e63a22d617d4d0756cca6695c.jpg"
+    class="object-contain w-full h-30 lg:h-72 mt-[4rem]"
+    alt="@KESHI"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <h1>@KESHI</h1>
+    
+
+    
+
+    
+  </div>
+</a><a class="relative block border bg-slate-200 border-gray-100 w-1/6" href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
+  >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
+
+  <img
+    class="object-contain w-full h-30 lg:h-72 mt-[4rem]"
+    src="https://media-exp1.licdn.com/dms/image/C4E03AQGBvBlLy-uljw/profile-displayphoto-shrink_200_200/0/1610509335728?e=1651104000&v=beta&t=nfpAtG9KtEb083UxET9aH9RNvZYOEiZszKFxuSxvgvw"
+    alt="@KESHI"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <h1>@alstn</h1>
+    
+
+    
+
+    
+
+    
+  </div>
+</a><a class="relative block border bg-slate-200 w-1/6 " href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
+  >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
+
+  <img
+    class="object-contain w-full h-30 lg:h-72 mt-[4rem]"
+    src="https://i.pinimg.com/originals/d8/67/4a/d8674a00506bcecfe82af05dcbb0cf17.jpg"
+    alt="Build Your Own Drone"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <h1>@alstn</h1>
+    
+
+    
+
+    
+
+    
+  </div>
+</a>
+<a class="relative block border bg-slate-200 mr-[5rem] w-1/6" href="/product/build-your-own-drone">
+  <button
+    class="absolute p-2 text-white bg-black rounded-full right-4 top-4"
+    type="button"
+  >
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+    </svg>
+  </button>
+
+  <img
+    class="object-contain w-full h-30 lg:h-72 mt-[4rem]"
+    src="https://i.pinimg.com/originals/c8/b6/d2/c8b6d20be240aa6e8667d9f383d9ed3a.jpg"
+    alt="Build Your Own Drone"
+    loading="lazy"
+  />
+
+  <div class="p-6">
+    <h1>@alstn</h1>
+    
+    
+
+    
+
+    
 
     
   </div>
