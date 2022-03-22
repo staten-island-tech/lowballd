628eb44ba9404a6db39e3b37f6b47bf1d3529d91
alston <alston.chan22@sitechhs.com>
2022-03-22 12:25:09 -0400

    changed title

--- lowballd/src/components/GlobalNavbar.vue
+++ lowballd/src/components/GlobalNavbar.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="w-full h-14 flex flex-row justify-between px-8 pt-4">
-      <h1 class="text-xl">Lowballd</h1>
+      <router-link to="/" class="text-xl">Lowballd</router-link>
       <div>
         <router-link to="feed" class="px-6">Feed</router-link>
         <router-link to="marketplace" class="px-6">Marketplace</router-link>
