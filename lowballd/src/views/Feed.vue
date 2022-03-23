714975ed9aaadcb90f7ada7d9b8a7a361bb6a5b0
vincentvvs <vincent.shi22@sitechhs.com>
2022-03-23 12:25:27 -0400

    component done

--- lowballd/src/views/Feed.vue
+++ lowballd/src/views/Feed.vue
@@ -8,6 +8,7 @@
 import GlobalNavbar from "../components/GlobalNavbar.vue";
 import FeedComponent from '../components/Feed/FeedComponent.vue'
 
+
 export default {
     name: 'Home',
     components: {
