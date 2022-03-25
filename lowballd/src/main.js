4e94c766f6b21ac8676ca8174f611c3074cad89a
Carol-Student22 <carol.lian22@gmail.com>
2022-03-25 12:18:49 -0400

    cannot figure out vuex with vue 3

--- lowballd/src/main.js
+++ lowballd/src/main.js
@@ -4,18 +4,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-// Import the Auth0 configuration
-
 import authConfig from "../auth_config.json";
 import { setupAuth } from "./auth";
 
+import { createApp } from "vue";
+import { createStore } from "vuex";
+// Create a new store instance or import from module.
+const store = createStore({
+  /* state, actions, mutations */
+});
+const app = createApp();
+app.use(store);
+app.mount("#app");
 
-// import store form './store'
-// new Vue({
-//   store,
-//   // existing Vue settings here...
-// }).mount('#app')   // or whatever mount point you have
 
 let app = createApp(App).use(store).use(router);
 
