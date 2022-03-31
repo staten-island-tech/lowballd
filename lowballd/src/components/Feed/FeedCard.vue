569bd1edfcee601defe3a171e3bce5eb0071f3b7
vincentvvs <vincent.shi22@sitechhs.com>
2022-03-31 12:26:16 -0400

    ok

--- lowballd/src/components/Feed/FeedCard.vue
+++ lowballd/src/components/Feed/FeedCard.vue
@@ -1,14 +1,17 @@
 <template>
-<div class="flex justify-center items-center min-h-full bg-slate-200">
-  <div class="position: relative;">
-    <div >
-      <div>
-        <div>
+<body class="flex justify-center items-center min-h-full">
+  <div class="position: relative; h-[400px] w-[360px] bg-slate-200 shadow-slate-500 p-[20px]">
+    <div class="flex justify-content: space-between items-center">
+      <div class="flex items-center">
+        <div class="relative"> 
+          <div>
+            <img>
+          </div>
         </div>
       </div>
-    </div>
-  </div> 
-</div>
+    </div> 
+  </div>
+</body>
 </template>
 
 
