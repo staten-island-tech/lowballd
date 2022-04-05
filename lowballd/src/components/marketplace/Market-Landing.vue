2c002a52edc17ea1eff89df7a02faa5a996d8eaa
Carol-Student22 <carol.lian22@gmail.com>
2022-04-05 12:26:49 -0400

    got it working - took too long

--- lowballd/src/components/marketplace/Market-Landing.vue
+++ lowballd/src/components/marketplace/Market-Landing.vue
@@ -2,30 +2,22 @@
     <section class="bg-white my-7 mx-5 sm:mx-0 flex flex-row sm:flex-col justify-center w-full ">
         <Filter />
         <div class='bg-white sm:justify-center max-w-screen-xl mx-3 flex flex-row flex-wrap '>
-            <Card v-for="product in products" 
-                :product="product"
-                :name="product.product_title"
-                :price="product.product_price"
-                :imageUrl="product.product_img"
-            />
+            <Card />
+
         </div>
     </section>
 </template>
 
 <script>
-import Card from './Card.vue';
 import Filter from './Filter.vue';
+import Card from './Card.vue';
 
 export default {
     name: 'MarketLanding',
-    components: {
-        Card, Filter,
+    components: { 
+        Card,
+        Filter,
     },
-    created() {
-        console.log(this.$store.state.products)
-    },
-    
-    
     
 }
 
--- lowballd/src/store/index.js
+++ lowballd/src/store/index.js
@@ -17,22 +17,47 @@ const store = createStore({
         product_shipping: "",
       },
       {
-        product_title: "vuex Double Knee Cargo Tactical Pants",
-        product_img: "https://media-photos.depop.com/b0/27687955/1173757715_0f031502cba145aea2d146dafb6fb8b3/P0.jpg",
-        product_price: "79.00",
-        product_style: "Pant",
+        product_title: "Dr. Marten Gray Suede Industrial Boots",
+        product_img: "https://media-photos.depop.com/b0/14864161/1181924176_4c27d7c0143349eda377a76893e71d4d/P0.jpg",
+        product_price: "89.00",
+        product_style: "Shoes",
         product_size: "",
         product_brand: "",
         product_condition: "",
         product_color: "",
         product_shipping: "",
       },
+      {
+        product_title: "Hard Rock Niagara Falls Tee",
+        product_img: "https://media-photos.depop.com/b0/33314125/1183416694_7d34845ebc5143fbb67cd568b44b9fd3/P0.jpg",
+        product_price: "15.00",
+        product_style: "Shirts",
+        product_size: "Small",
+        product_brand: "",
+        product_condition: "",
+        product_color: "",
+        product_shipping: "Varies",
+      },
+      {
+        product_title:"Fun Product That Does Something Cool",
+        product_img: "https://images.unsplash.com/photo-1627844541143-a561a1a9b72a",
+        product_price: "125.00",
+        product_style:  "High Top Jordan Air",
+        product_size: "Small",
+        product_brand: "Nike",
+        product_condition: "Brand New",
+        product_color: "Red-Black",
+        product_shipping: "Free",
+      },
 
-    ], cart: []
+    ], 
+    cart: [],
+    chosenOne:[],
   },
+
 
 
 });
 
-export default store
+export default store;
 
