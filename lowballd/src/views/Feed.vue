c99f64534474c87f0ba64cc030b0470f18ed9a7f
maxlivit <maxim.livit22@sitechhs.com>
2022-04-08 09:36:08 -0400

    you're welcome vincent

--- lowballd/src/views/Feed.vue
+++ lowballd/src/views/Feed.vue
@@ -5,10 +5,7 @@
 <h1 class="text-center text-3xl font-['Spectral'] font-semibold">WHAT DID YOU WEAR TODAY?</h1>
 <div class="flex items-center justify-center"><button class=" mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current hover:font-bold cursor-pointer select-none">Trending</button><button class="mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current cursor-pointer hover:font-bold select-none">Following</button></div>
 </div>
-<FeedCard name="Keshi" />
-<FeedCard name="Bruce" />
-<FeedCard name="Clark" />
-<FeedCard name="Diana" />
+<FeedCard></FeedCard>
 
 
 </template>
