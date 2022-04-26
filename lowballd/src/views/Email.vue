f82b5b1dbd7a1eb5ea4ad11776b56178eaf4e37e
vincentvvs <vincent.shi22@sitechhs.com>
2022-04-26 12:28:46 -0400

    ok
    ok

---
+++ lowballd/src/views/Email.vue
@@ -0,0 +1,16 @@
+<template>
+<GlobalNavbar/>
+<div class="min-h-screen">
+<h1 class="text-center text-4xl mt-[20%]">Thank you for verifying your email</h1>
+</div>
+</template>
+
+<script>
+import GlobalNavbar from "../components/GlobalNavbar.vue";
+export default {
+    name: 'Email',
+    components: {
+        GlobalNavbar
+    },
+} 
+</script>
