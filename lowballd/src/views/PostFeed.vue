1338cfa316cbcb1ad8d925f2c8ac5deb78f7decc
alston <alston.chan22@sitechhs.com>
2022-04-26 11:15:32 -0400

    post feed done

--- lowballd/src/views/PostFeed.vue
+++ lowballd/src/views/PostFeed.vue
@@ -38,21 +38,9 @@
 
             <div>
               <label class="block text-sm font-medium text-gray-700"> Add photos </label>
-              <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                <div class="space-y-1 text-center">
-                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                    <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
-                    <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
+
+                <UploadImages :max="5" maxError="Maximum of 5 files." clearAll="Clear All"/>
+
             </div>
           </div>
           <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
@@ -71,11 +59,12 @@
 <script>
 import GlobalNavbar from "../components/GlobalNavbar.vue";
 import Datepicker from '@themesberg/tailwind-datepicker/Datepicker';
+import UploadImages from "../components/postfeed/vue-upload-drop-images.vue"
 
 export default {
   name: "PostFeed",
   components: {
-    GlobalNavbar,
+    GlobalNavbar, UploadImages
   },
   mounted() {
     const datepickerEl = document.getElementById('datepickerId');
