e4c597c91bfd4aebe2f4e5b91fce420278ca876a
alston <alston.chan22@sitechhs.com>
2022-05-10 12:00:09 -0400

    location

--- lowballd/src/components/profile/ManageAccount.vue
+++ lowballd/src/components/profile/ManageAccount.vue
@@ -10,8 +10,8 @@
         </div>
         
         <div class="flex w-1/2 justify-around ml-8">
-            <label for="pronouns" class="text-lg mr-2 flex flex-col justify-center">Pronouns</label>
-            <input id="pronouns" class="flex-grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="he/him, she/her, they/them, etc" required>
+            <label for="pronouns" class="text-lg mr-2 flex flex-col justify-center">Location</label>
+            <input id="pronouns" class="flex-grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ex. New York, NY" required>
         </div>
     </div>
     <p class="text-lg ml-10">Description</p>
