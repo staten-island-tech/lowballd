e7b6d15edefd679ed59c2d4f874ba1db5c06248b
maxlivit <maxim.livit22@sitechhs.com>
2022-05-16 12:18:51 -0400

    contact

--- lowballd/src/views/Contact.vue
+++ lowballd/src/views/Contact.vue
@@ -19,10 +19,11 @@
         <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Reason for Email</label>
         <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option selected="">Choose the most related topic.</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
+        <option value="Ac">Account Information</option>
+        <option value="Sh">Shipping Problem</option>
+        <option value="Us">Issue With User</option>
+        <option value="Po">Posts Not Working</option>
+        <option value="Ot">Other</option>
         </select>
     </div>
     <div class="mb-6">
