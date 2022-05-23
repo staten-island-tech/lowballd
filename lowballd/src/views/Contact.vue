56d7794c94330b147f16104f643a1c5e464c018f
maxlivit <maxim.livit22@sitechhs.com>
2022-05-23 12:14:38 -0400

    contact

---
+++ lowballd/src/views/Contact.vue
@@ -0,0 +1,48 @@
+<template>
+<GlobalNavbar></GlobalNavbar>
+<div class="w-2/3 mx-auto pb-36">
+    <div class="grid xl:grid-cols-2 xl:gap-6">
+        <div class="relative z-0 w-full mb-6 group">
+            <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+        </div>
+        <div class="relative z-0 w-full mb-6 group">
+            <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+        </div>
+    </div>
+    <div class="relative z-0 w-full mb-6 group">
+        <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+    </div>
+    <div class="mb-6">
+        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Reason for Email</label>
+        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <option selected="">Choose the most related topic.</option>
+        <option value="Ac">Account Information</option>
+        <option value="Sh">Shipping Problem</option>
+        <option value="Us">Issue With User</option>
+        <option value="Po">Posts Not Working</option>
+        <option value="Ot">Other</option>
+        </select>
+    </div>
+    <div class="mb-6">
+        <label for="about" class="block text-sm font-medium text-gray-900 dark:text-gray-300">What can we help you with?</label>
+        <div class="mt-1">
+            <textarea id="about" name="about" rows="10" class="text-gray-900 shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-lg bg-gray-50 sm:text-md" placeholder="Provide a detailed description of your issue or concern."></textarea>
+        </div>
+    </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+</div>
+<Footer></Footer>
+</template>
+<script>
+import GlobalNavbar from "../components/GlobalNavbar.vue";
+import Footer from "../components/footer/Footer.vue";
+export default {
+    name: 'Contact',
+    components: {
+        GlobalNavbar, Footer,
+    },
+};
+</script>
