35672baba46a7f0198b2431da735f1005a1a5ffe
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-05-25 22:57:00 -0400

    added modal to some of the routerlinks to encourage people to sign in

--- lowballd/src/main.js
+++ lowballd/src/main.js
@@ -4,12 +4,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import 'flowbite';
+import "flowbite";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 import authConfig from "../auth_config.json";
 import { setupAuth } from "./auth";
 
 let app = createApp(App).use(store).use(router);
+app.use(VueSweetalert2);
 
 function callbackRedirect(appState) {
   router.push(appState && appState.targetUrl ? appState.targetUrl : "/");
@@ -19,5 +22,4 @@ setupAuth(authConfig, callbackRedirect).then((auth) => {
   app.use(auth).mount("#app");
 });
 
-
-app.use(store)
+app.use(store);
