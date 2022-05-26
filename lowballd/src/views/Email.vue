5d17ad6f221d6171861133478f955b637c1d0448
vincentvvs <vincent.shi22@sitechhs.com>
2022-05-26 12:28:30 -0400

    ok
    ok

--- lowballd/src/views/Email.vue
+++ lowballd/src/views/Email.vue
@@ -2,9 +2,13 @@
 <GlobalNavbar/>
 <div class="min-h-screen ">
 <h1 class="text-center text-4xl mt-[20%]">Thank you for verifying your email</h1>
+
+<a href="http://localhost:3000/">
 <button class="ml-[46.5%] mr-[46.5 %] mt-[2%] bg-slate-400 hover:bg-blue-200 text-white font-bold py-2 px-4 rounded">
-  Take Me Back
+
+  Take Me Back 
 </button>
+</a>
 </div>
 
 </template>
---
+++ lowballd/src/views/Faq.vue
@@ -0,0 +1,22 @@
+<template>
+<GlobalNavbar/>
+<div>
+<h1 class="text-center text-3xl mt-20 font-['Spectral'] font-semibold">Frequently Asked Questions</h1>
+<h1 class="text-center text-xl mt-20 font-['Spectral'] font-semibold">Do you have any questions about Lowballd? See the list below for our most frequently asked questions. If your question is not here, please contact us.</h1>
+</div>
+<div class="">
+<h1 class="mt-[10%] ml-[10%] text-2xl font-['Spectral'] font-semibold">Shipment Policy</h1>
+</div>
+
+
+</template>
+
+<script>
+import GlobalNavbar from "../components/GlobalNavbar.vue";
+export default {
+    name: 'Faq',
+    components: {
+        GlobalNavbar
+    },
+} 
+</script>
