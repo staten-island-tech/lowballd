a5bc841ff7b7c7dc662a401feab3a94db5f368d5
alston <alston.chan22@sitechhs.com>
2022-05-31 11:46:22 -0400

    feed modal done

--- lowballd/src/views/ViewProfile.vue
+++ lowballd/src/views/ViewProfile.vue
@@ -4,7 +4,7 @@
     <div class="flex flex-row justify-center w-4/5 mx-auto p-10">
         <img class="rounded w-48 h-48" src="https://3.files.edl.io/aeb1/20/12/02/154937-46cc468f-b7f4-4bb3-945e-3265bdb605d4.jpg" alt="Extra large avatar">
         <div class="flex flex-col justify-center w-1/2 ml-20">
-            <p class="font-bold text-3xl">mwhalen3</p>
+            <p class="font-bold text-3xl">{{ $route.params.userId }}</p>
             <p class="text-md text-slate-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <div class="flex flex-row mt-6">
                 <p class="text-lg text-slate-700 pr-4"><span class="font-bold">25</span> Posts</p>
