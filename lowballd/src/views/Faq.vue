a74e8ff133d21b0d49d3f5b4ab2880908ca9268e
vincentvvs <vincent.shi22@sitechhs.com>
2022-06-05 12:34:01 -0400

    Update Faq.vue

--- lowballd/src/views/Faq.vue
+++ lowballd/src/views/Faq.vue
@@ -3,15 +3,15 @@
 <section class="text-gray-700">
         <div class="container px-5 py-24 mx-auto">
           <div class="text-center mb-20 ">
-            <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
+            <h1 class=" text-2xl font-medium text-center title-font text-gray-900 mb-4">
               Frequently Asked Question
             </h1>
-            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            <p class="text-base leading-relaxed mx-auto">
               The most common questions about how our business works and what
               can do for you.
             </p>
           </div>
-          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2 border-2 border-white-900 p-[1%]">
+          <div class="flex flex-wrap -mx-2 border-2 border-white-900 p-[1%]">
           
           
             <div class="w-full px-4 py-2"><h1 class="text-xl">Shipping</h1>
