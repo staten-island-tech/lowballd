137111d007ed88c18d5b091e357aad005a377cd0
vincentvvs <vincent.shi22@sitechhs.com>
2022-06-05 13:09:00 -0400

    Update Home.vue

--- lowballd/src/views/Home.vue
+++ lowballd/src/views/Home.vue
@@ -49,7 +49,7 @@
           </p>
 
           <a
-            class="inline-block px-12 py-3 mt-8 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring"
+            class="inline-block px-12 py-3 mt-8 font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring"
            
           >
             
