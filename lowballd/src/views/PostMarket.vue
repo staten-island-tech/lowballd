606787c08fb06bbda778893d295f0844e0abc001
vincentvvs <vincent.shi22@sitechhs.com>
2022-06-05 04:30:00 -0400

    k

--- lowballd/src/views/PostMarket.vue
+++ lowballd/src/views/PostMarket.vue
@@ -5,7 +5,6 @@
       <div class="md:grid md:grid-cols-3 md:gap-6">
         <div class="mt-5 md:mt-0 md:col-span-2">
           <form
-            @submit="postData"
             @submit.prevent="postData"
             action="#"
             method="POST"
