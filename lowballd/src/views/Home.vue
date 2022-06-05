135aad0a90b9cb152757f7460186b526475ec43f
vincentvvs <vincent.shi22@sitechhs.com>
2022-06-05 18:21:09 -0400

    Update Home.vue

--- lowballd/src/views/Home.vue
+++ lowballd/src/views/Home.vue
@@ -41,7 +41,7 @@
           </p>
           <a
           class="inline-flex items-center px-8 py-3 mt-8 text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring cursor-pointer">
-          <span class="text-sm font-medium"> <a @click="login()">Get started</a></span>
+          <span class="text-sm font-medium" @click="login()"> Get started</span>
         </a>
         </div>
       </div>
