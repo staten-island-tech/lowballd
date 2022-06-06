f769ce316af3a0d1dbc207e439ec825fd7a085ef
alston <alston.chan22@sitechhs.com>
2022-06-05 23:14:58 -0400

    marketplace edits

--- lowballd/src/components/product/ProductTest.vue
+++ lowballd/src/components/product/ProductTest.vue
@@ -1,349 +1,66 @@
-<!--
-  This component uses @tailwindcss/forms and @tailwindcss/aspect-ratio
-
-  yarn add @tailwindcss/forms @tailwindcss/aspect-ratio
-  npm install @tailwindcss/forms @tailwindcss/aspect-ratio
-
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio')]
--->
-
-<style>
-.no-spinners {
-  -moz-appearance: textfield;
-}
-
-.no-spinners::-webkit-outer-spin-button,
-.no-spinners::-webkit-inner-spin-button {
-  margin: 0;
-  -webkit-appearance: none;
-}
-</style>
-
 <template>
-  <section>
-    <div class="relative max-w-screen-xl px-4 py-8 mx-auto">
-      <div class="grid items-start grid-cols-1 gap-8 md:grid-cols-2">
-        <div class="grid grid-cols-2 gap-4 md:grid-cols-1">
-          <div class="aspect-w-1 aspect-h-1">
-            <img
-              alt="Mobile Phone Stand"
-              class="object-cover rounded-xl w-auto max-h-100"
-              :src="prod.images"
-            />
-          </div>
+  <GlobalNavbar></GlobalNavbar>
+    <router-link to="/marketplace" class="mx-56">←	 Back to Marketplace</router-link>
+    <section class="text-gray-700 overflow-hidden bg-white">
+    <div class="container px-5 py-12 mx-auto">
+        <div class="lg:w-4/5 mx-auto flex flex-wrap">
+            <img :src="prod.images[0]" class="w-1/2 object-cover object-center rounded border border-gray-200">
+            <div class="w-1/2 lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <div class="flex items-center space-x-4 -mt-8 mb-6">
+                <img class="w-12 h-12 object-cover object-center rounded-full" :src="profile_picture" />
+                <h2 class="text-gray-800 text-xl font-bold cursor-pointer">{{ username }}</h2>
+              </div>
+                <h2 class="text-sm title-font text-gray-700 tracking-widest">{{ prod.category }}</h2>
+                <h1 class="text-gray-900 text-4xl title-font font-bold mb-1">{{ prod.title }}</h1>
+                <div class="flex mb-4">
+                    <span class="title-font font-medium text-2xl text-gray-900">${{ prod.price }}</span>
+                </div>
+                <p class="leading-relaxed my-4">{{ prod.description }}</p>
+                <div class="flex flex-row">
+                  <div class="mr-8">
+                    <p class="font-bold">Condition</p>
+                    <p>{{ prod.condition }}</p>
+                  </div>
+                  <div>
+                    <p class="font-bold">Listed Since</p>
+                    <p>{{ createdDate }}</p>
+                  </div>
+                </div>
+                <div class="flex border-t border-gray-300 mt-5 pt-5">
+                    <button type="submit" class="bg-gray-800 duration-200 focus:outline-none focus:shadow-outline font-medium h-12 hover:bg-gray-900 inline-flex items-center justify-center px-6 text-white tracking-wide transition w-full">
+                        Message Seller
+                    </button>
+                </div>
+            </div>
         </div>
-
-        <div class="sticky top-0">
-          <div class="flex flex-row">
-            <div class="avatar pt-4">
-              <div
-                class="rounded-full w-24 h-24 ring ring-primary ring-offset-base-100 ring-offset-2"
-              >
-                <img
-                  src="https://media-exp1.licdn.com/dms/image/C4E03AQGBvBlLy-uljw/profile-displayphoto-shrink_200_200/0/1610509335728?e=1648684800&v=beta&t=u9GCzx6GctOLBqOeRB1Z9Jv7aBXeb8XZrHCyvFBA-qw"
-                />
-              </div>
-            </div>
-            <div class="p-4">
-              <h1>Alston Chan</h1>
-              <p class="text-slate-500 text-sm">Staten Island, NY</p>
-              <div class="rating">
-                <input
-                  type="radio"
-                  name="rating-2"
-                  class="mask mask-star-2 bg-warning"
-                />
-                <input
-                  type="radio"
-                  name="rating-2"
-                  class="mask mask-star-2 bg-warning"
-                />
-                <input
-                  type="radio"
-                  name="rating-2"
-                  class="mask mask-star-2 bg-warning"
-                />
-                <input
-                  type="radio"
-                  name="rating-2"
-                  checked="checked"
-                  class="mask mask-star-2 bg-warning"
-                />
-                <input
-                  type="radio"
-                  name="rating-2"
-                  class="mask mask-star-2 bg-warning"
-                />
-                <p class="pl-2 pt-[3px]">{{ prod.likes }}</p>
-              </div>
-              <!--<a href="#" class="text-blue-600 visited:text-purple-600">Message Seller</a>-->
-            </div>
-          </div>
-
-          <div class="flex justify-between mt-8">
-            <div class="max-w-[35ch]">
-              <h1 class="text-2xl font-bold">{{ prod.title }}</h1>
-              <p class="mt-0.5 text-sm">Highest Rated Product</p>
-              <div class="flex mt-2 -ml-0.5">
-                <svg
-                  class="w-5 h-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-
-                <svg
-                  class="w-5 h-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-
-                <svg
-                  class="w-5 h-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-
-                <svg
-                  class="w-5 h-5 text-yellow-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-
-                <svg
-                  class="w-5 h-5 text-gray-200"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <p class="text-lg font-bold">
-              {{ prod.price }}
-            </p>
-          </div>
-
-          <details class="relative mt-4 group">
-            <summary class="block">
-              <div>
-                <div class="prose max-w-none group-open:hidden">
-                  <p>
-                    {{ prod.description }}
-                  </p>
-                </div>
-
-                <span
-                  class="mt-4 text-sm font-medium underline cursor-pointer group-open:absolute group-open:bottom-0 group-open:left-0 group-open:mt-0"
-                >
-                  Read More
-                </span>
-              </div>
-            </summary>
-
-            <div class="pb-6 prose max-w-none">
-              <p>{{ prod.description }}</p>
-              <div class="flex flex-col w-full pt-12">
-                <div class="flex justify-between">
-                  <div class="grid font-bold">Size</div>
-                  <h1 class="text-slate-700 text-right">{{ prod.size }}</h1>
-                </div>
-                <div class="divider"></div>
-                <div class="flex justify-between">
-                  <div class="grid font-bold">Brand</div>
-                  <h1 class="text-slate-700 text-right">{{ prod.brand }}</h1>
-                </div>
-                <div class="divider"></div>
-                <div class="flex justify-between">
-                  <div class="grid font-bold">Condition</div>
-                  <h1 class="text-slate-700 text-right">
-                    {{ prod.condition }}
-                  </h1>
-                </div>
-                <div class="divider"></div>
-                <div class="flex justify-between">
-                  <div class="grid font-bold">Style</div>
-                  <h1 class="text-slate-700 text-right">{{ prod.style }}</h1>
-                </div>
-                <div class="divider"></div>
-                <div class="flex justify-between">
-                  <div class="grid font-bold">Color</div>
-                  <h1 class="text-slate-700 text-right">{{ prod.color }}</h1>
-                </div>
-                <div class="divider"></div>
-                <div class="flex justify-between">
-                  <div class="grid font-bold">Shipping</div>
-                  <h1 class="text-slate-700 text-right">{{ prod.ship }}</h1>
-                </div>
-                <div class="divider"></div>
-              </div>
-            </div>
-          </details>
-
-          <form class="mt-8">
-            <fieldset>
-              <legend class="mb-1 text-sm font-medium">Tag</legend>
-
-              <div class="flow-root">
-                <div class="flex flex-wrap -m-0.5">
-                  <label for="color_tt" class="cursor-pointer p-0.5">
-                    <input
-                      type="radio"
-                      name="color"
-                      id="color_tt"
-                      class="sr-only peer"
-                    />
-
-                    <span
-                      v-for="tag in prod.tags"
-                      class="inline-block px-3 py-1 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white"
-                    >
-                      {{ tag }}
-                    </span>
-                  </label>
-                </div>
-              </div>
-            </fieldset>
-
-            <fieldset class="mt-4">
-              <legend class="mb-1 text-sm font-medium">Size</legend>
-
-              <div class="flow-root">
-                <div class="flex flex-wrap -m-0.5">
-                  <label for="size_xs" class="cursor-pointer p-0.5">
-                    <input
-                      type="radio"
-                      name="size"
-                      id="size_xs"
-                      class="sr-only peer"
-                    />
-
-                    <span
-                      class="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white"
-                    >
-                      XS
-                    </span>
-                  </label>
-
-                  <label for="size_s" class="cursor-pointer p-0.5">
-                    <input
-                      type="radio"
-                      name="size"
-                      id="size_s"
-                      class="sr-only peer"
-                    />
-
-                    <span
-                      class="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white"
-                    >
-                      S
-                    </span>
-                  </label>
-
-                  <label for="size_m" class="cursor-pointer p-0.5">
-                    <input
-                      type="radio"
-                      name="size"
-                      id="size_m"
-                      class="sr-only peer"
-                    />
-
-                    <span
-                      class="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white"
-                    >
-                      M
-                    </span>
-                  </label>
-
-                  <label for="size_l" class="cursor-pointer p-0.5">
-                    <input
-                      type="radio"
-                      name="size"
-                      id="size_l"
-                      class="sr-only peer"
-                    />
-
-                    <span
-                      class="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white"
-                    >
-                      L
-                    </span>
-                  </label>
-
-                  <label for="size_xl" class="cursor-pointer p-0.5">
-                    <input
-                      type="radio"
-                      name="size"
-                      id="size_xl"
-                      class="sr-only peer"
-                    />
-
-                    <span
-                      class="inline-flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full group peer-checked:bg-black peer-checked:text-white"
-                    >
-                      XL
-                    </span>
-                  </label>
-                </div>
-              </div>
-            </fieldset>
-
-            <div class="flex mt-8">
-              <div>
-                <label for="quantity" class="sr-only">Qty</label>
-
-                <input
-                  type="number"
-                  id="quantity"
-                  min="1"
-                  value="1"
-                  class="w-12 py-3 text-xs text-center border-gray-200 rounded no-spinners"
-                />
-              </div>
-
-              <button
-                type="submit"
-                class="block px-5 py-3 ml-3 text-xs font-medium text-white bg-blue-600 rounded hover:bg-pink-500"
-              >
-                Add to Cart
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
+</section>
+
+  <section class="text-gray-700 overflow-hidden bg-white">
+      <div class="container px-5 py-12 mx-auto">
+        <p class="text-2xl mx-32">You might also like</p>
+      </div>
   </section>
+
+  <Footer></Footer>
 </template>
 
 <script>
+import GlobalNavbar from "../GlobalNavbar.vue";
+import Footer from "../footer/Footer.vue";
+
+
 export default {
   name: "productTest",
+  components: {
+    GlobalNavbar, Footer
+  },
   data() {
     return {
       prod: {},
+      createdDate: null,
+      username: null,
+      profile_picture: null,
     };
   },
   methods: {
@@ -355,13 +72,32 @@ export default {
       );
       const data = await response.json();
       this.prod = data;
-      console.log(data.userId);
-    },
 
+      let month = this.prod.createdAt.slice(5, 7);
+      let day = this.prod.createdAt.slice(8, 10);
+      let year = this.prod.createdAt.slice(0, 4);
+      this.createdDate = `${month}/${day}/${year}`;
+    },
+    async getUserInfo() {
+      try {
+        const response = await fetch(
+          `https://lowballd-backend.onrender.com/api/user/${this.prod.userId}`
+        );
+        const data = await response.json();
+        this.username = data.username;
+        this.profile_picture = data.profile_picture;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
   created() {
     this.getDataById();
-  }
+  },
+  mounted() {
+    this.getUserInfo();
+  },
   
 };
 </script>
+
