f37330319fcc62dbce74f25f47d427887f34c753
vincentvvs <vincent.shi22@sitechhs.com>
2022-06-06 00:02:23 -0400

    Revert "Merge branch 'main' of https://github.com/staten-island-tech/fullstack-frontend-team-alst"
    This reverts commit c2f0f31c2ff31b1fdcebb824eacc78630b3c51a8, reversing
    changes made to db1e1903fe1ee6bb8d15c4fd130869fb44da4cd5.

--- lowballd/src/views/Feed.vue
+++ lowballd/src/views/Feed.vue
@@ -4,9 +4,20 @@
     <h1 class="text-center text-3xl mt-20 font-['Spectral'] font-semibold">
       OUTFIT OF THE DAY
     </h1>
-    <h1 class="text-center text-3xl font-['Spectral']">
+    <h1 class="text-center text-3xl font-['Spectral'] font-semibold">
       WHAT DID YOU WEAR TODAY?
     </h1>
+    <div class="flex items-center justify-center">
+      <button
+        class="mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current hover:font-bold cursor-pointer select-none"
+      >
+        Trending</button
+      ><button
+        class="mb-20 mt-6 mr-5 text-xl border-b-2 border-double border-transparent hover:border-current cursor-pointer hover:font-bold select-none"
+      >
+        Following
+      </button>
+    </div>
   </div>
   <div class="mx-auto flex flex-wrap">
     <FeedCard v-for="item in apiMessage"
