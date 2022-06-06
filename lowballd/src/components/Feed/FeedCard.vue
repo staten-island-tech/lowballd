52513e18d12318723904d14640ef99dd31836b2a
vincentvvs <vincent.shi22@sitechhs.com>
2022-06-06 03:14:37 -0400

    final home page i think

--- lowballd/src/components/Feed/FeedCard.vue
+++ lowballd/src/components/Feed/FeedCard.vue
@@ -122,7 +122,7 @@
             >
               <button
                 type="button"
-                class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-180 sm:text-sm sm:leading-5"
                 @click="showModal = false"
               >
                 Close
